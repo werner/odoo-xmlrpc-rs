1e62187156a30424e3bdf20dc2a2f82aea5560d1
werner <werner_a_e@yahoo.es>
2018-03-17 17:19:33 -0300

    Initial commit

---
+++ src/main_request.rs
@@ -0,0 +1,24 @@
+use xmlrpc::{Request, Value, Error};
+
+pub struct BasicRequest<'a> {
+  base_url: &'a str
+}
+
+impl<'a> BasicRequest<'a> {
+
+  pub fn new(url: &'a str) -> BasicRequest {
+    BasicRequest {
+      base_url: url
+    }
+  }
+
+  pub fn authenticate(&mut self, db: &str, username: &str, password: &str) -> Result<Value, Error> {
+    let request = Request::new("authenticate").arg(db).arg(username).arg(password);
+    self.execute_method(request) 
+  }
+
+  fn execute_method(&self, request: Request) -> Result<Value, Error> {
+    let full_url = format!("{}/xmlrpc/2/common", &self.base_url);
+    request.call_url(&full_url)
+  }
+}
